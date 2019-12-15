f980bfa2e5adb4bb7dee78956b5779978830a924
gongali1997 <u1939698@campus.udg.edu>
2019-12-15 18:17:14 +0100

    Plantilla fotos correcte

--- geophoto/app/src/main/java/org/udg/pds/todoandroid/activity/ShowImages.java
+++ geophoto/app/src/main/java/org/udg/pds/todoandroid/activity/ShowImages.java
@@ -54,12 +54,10 @@ public class ShowImages extends AppCompatActivity {
         imageList.add("https://farm3.staticflickr.com/2917/14144166232_44613c53c7_c.jpg");
         imageList.add("https://farm8.staticflickr.com/7453/13960410788_3dd02b7a02_c.jpg");
         imageList.add("https://farm1.staticflickr.com/920/29297133218_de38a7e4c8_c.jpg");
-        imageList.add("https://farm2.staticflickr.com/1788/42989123072_6720c9608d_c.jpg");
         imageList.add("https://farm1.staticflickr.com/888/29062858008_89851766c9_c.jpg");
         imageList.add("https://farm2.staticflickr.com/1731/27940806257_8067196b41_c.jpg");
         imageList.add("https://farm1.staticflickr.com/884/42745897912_ff65398e38_c.jpg");
         imageList.add("https://farm2.staticflickr.com/1829/27971893037_1858467f9a_c.jpg");
-        imageList.add("https://farm2.staticflickr.com/1822/41996470025_414452d7a0_c.jpg");
         imageList.add("https://farm2.staticflickr.com/1793/42937679651_3094ebb2b9_c.jpg");
         imageList.add("https://farm1.staticflickr.com/892/42078661914_b940d96992_c.jpg");
 
