9b367088a672c4e64c482e8d7b6d8200cf27f994
gongali1997 <u1939698@campus.udg.edu>
2019-12-17 16:36:12 +0100

    Arreglat camp user

--- geophoto/app/src/main/java/org/udg/pds/todoandroid/entity/User.java
+++ geophoto/app/src/main/java/org/udg/pds/todoandroid/entity/User.java
@@ -1,8 +1,10 @@
 package org.udg.pds.todoandroid.entity;
 
+import java.io.Serializable;
+
 /**
  * Created by imartin on 12/02/16.
  */
-public class User {
+public class User  implements Serializable  {
   public String token;
 }
---
+++ geophoto/app/src/main/java/org/udg/pds/todoandroid/entity/Usuari.java
@@ -0,0 +1,17 @@
+package org.udg.pds.todoandroid.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by imartin on 12/02/16.
+ */
+public class Usuari implements Serializable {
+  public String username;
+
+
+  public Usuari(String s1)
+  {
+    username=s1;
+  }
+
+}
